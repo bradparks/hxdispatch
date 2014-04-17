9847d4194f857fcb8d921cb370725540e66c3385
Michel Käser <mk@frontender.ch>
2014-04-18 00:12:17 +0200

    that is what I wanted

--- src/hxdispatch/async/Promise.hx
+++ src/hxdispatch/async/Promise.hx
@@ -68,7 +68,7 @@ class Promise<T> extends hxdispatch.concurrent.Promise<T>
     /**
      * @{inherit}
      */
-    override private function executeCallbacks(callbacks:Iterable<Callback<T>>, arg:T):Void
+    override private function executeCallbacks(callbacks:Array<Callback<T>>, arg:T):Void
     {
         var callback:Callback<T>;
         if (callbacks.length == 0) {
